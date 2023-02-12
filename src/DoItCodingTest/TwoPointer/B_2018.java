6b856b038590854cc3b48200bf08127715fc5947
alexgim <alexgim@naver.com>
2023-02-13 00:24:43 +0900

    연속된 자연수의 합 주석 추가

--- src/DoItCodingTest/TwoPointer/B_2018.java
+++ src/DoItCodingTest/TwoPointer/B_2018.java
@@ -17,12 +17,12 @@ public class B_2018 {
             arr[i] = i;
         }
 
-        int count = 1;
+        int count = 1; // N은 기본으로 포함한다는 의미 -> 투 포인터로 잡기 힘듬
 
         int left = 1;
-        int right = 1;
-        int sum = arr[left];
-        while (right < N) {
+        int right = 1;         // 1 <= N 이라는 조건 때문에 2를 넣을 수 없음 -> 바로 index에러 발생
+        int sum = arr[left];   // left == right 상태는 존재 할 수 없기 때문에 처음에 arr[left]만 더함
+        while (right < N) {    // right = N 이 되면 left에 어떤 값이 와도 count++ 할 수 없기 때문
             if(sum == N) {
                 count++;
                 right++;
